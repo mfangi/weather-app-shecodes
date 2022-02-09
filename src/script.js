a8b6ac7e5fe33ae5274578190e4e0cc629910d14
mfangi <mariafeangela.boco@gmail.com>
2022-02-09 19:17:09 +0100

    Reorganized JS, implemented one-click unit converter

--- src/script.js
+++ src/script.js
@@ -14,18 +14,21 @@ let hour = currentDate.getHours();
 let minutes = ("0" + currentDate.getMinutes()).slice(-2);
 let correctDate = document.querySelector(".current-date");
 correctDate.innerHTML = `${day} ${hour}:${minutes}`;
-// City
+// Variables for search engine
+let apiKey = "9706500ac8f554aa37ba55acbe2d7310";
+let displayedCity = document.querySelector(".current-city");
+let displayedTemp = document.querySelector("#cityTemp");
 let form = document.querySelector("form");
+let geoLocButton = document.querySelector(".geoLocButton");
+// City
+
 function updateCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  let displayedCity = document.querySelector(".current-city");
-  displayedCity.innerHTML = `${cityInput.value.toUpperCase()}`;
+  displayedCity.innerHTML = `${cityInput.value}`;
   let typedCity = displayedCity.innerHTML;
-  let apiKey = "9706500ac8f554aa37ba55acbe2d7310";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${typedCity}&units=metric&appid=${apiKey}`;
   function showCityTemp(response) {
-    let displayedTemp = document.querySelector("#cityTemp");
     displayedTemp.innerHTML = Math.round(response.data.main.temp);
   }
   axios.get(apiUrl).then(showCityTemp);
@@ -33,21 +36,18 @@ function updateCity(event) {
 form.addEventListener("submit", updateCity);
 
 // Geolocalization
-let geoLocButton = document.querySelector(".geoLocButton");
+
 function handlePosition(position) {
   navigator.geolocation.getCurrentPosition(handlePosition);
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let apiKey = "9706500ac8f554aa37ba55acbe2d7310";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}
 &lon=
 ${lon}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(geoLoc);
 }
 function geoLoc(response) {
-  let displayedCity = document.querySelector(".current-city");
   displayedCity.innerHTML = response.data.name;
-  let displayedTemp = document.querySelector("#cityTemp");
   displayedTemp.innerHTML = Math.round(response.data.main.temp);
 }
 geoLocButton.addEventListener("click", handlePosition);
@@ -58,8 +58,10 @@ function convTempF(event) {
   let cityTempF = Math.round((cityTempNum * 9) / 5 + 32);
   let cityTemp = document.querySelector("#cityTemp");
   cityTemp.innerHTML = `${cityTempF}`;
-  degreesF.style.color = "grey";
-  degreesC.style.color = "white";
+  degreesF.style.color = "#1e78b1";
+  degreesC.style.color = "grey";
+  degreesF.style.pointerEvents = "none";
+  degreesC.style.pointerEvents = "all";
 }
 let degreesF = document.querySelector(".degrees.F");
 degreesF.addEventListener("click", convTempF);
@@ -70,8 +72,10 @@ function convTempC(event) {
   let cityTempC = Math.round(((cityTempNum - 32) * 5) / 9);
   let cityTemp = document.querySelector("#cityTemp");
   cityTemp.innerHTML = `${cityTempC}`;
-  degreesC.style.color = "grey";
-  degreesF.style.color = "white";
+  degreesC.style.color = "#1e78b1";
+  degreesF.style.color = "grey";
+  degreesC.style.pointerEvents = "none";
+  degreesF.style.pointerEvents = "all";
 }
 let degreesC = document.querySelector(".degrees.C");
 degreesC.addEventListener("click", convTempC);
