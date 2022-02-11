6e5d092f0e4ffff4f1cb95bdd19bf617fba0e8b5
mfangi <mariafeangela.boco@gmail.com>
2022-02-11 20:21:05 +0100

    Added JS forecast template

--- src/index.js
+++ src/index.js
@@ -22,6 +22,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -115,5 +144,7 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let geoLocate = document.querySelector("#geoLocButton");
 geoLocate.addEventListener("click", locate);
+
+displayForecast();
 
 search("Rome");
