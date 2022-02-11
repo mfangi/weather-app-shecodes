00e2ef0c64b5e319b3addb2d2f98ee4cfa362ce6
mfangi <mariafeangela.boco@gmail.com>
2022-02-11 20:34:33 +0100

    Added API structure in JS

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -51,6 +52,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9706500ac8f554aa37ba55acbe2d7310";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -77,6 +85,8 @@ function displayTemperature(response) {
     cityElement.style.fontSize = "30px";
   }
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -144,7 +154,5 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let geoLocate = document.querySelector("#geoLocButton");
 geoLocate.addEventListener("click", locate);
-
-displayForecast();
 
 search("Rome");
