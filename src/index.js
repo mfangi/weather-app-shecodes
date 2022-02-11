a78da9c91d220d0b41885907fa46a2b02f137ced
mfangi <mariafeangela.boco@gmail.com>
2022-02-11 21:37:44 +0100

    Delete console.log

--- src/index.js
+++ src/index.js
@@ -69,7 +69,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "9706500ac8f554aa37ba55acbe2d7310";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
